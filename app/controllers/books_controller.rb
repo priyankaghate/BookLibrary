975577565fcbc7daec30f0fb3f78e0161afee6a3
Tiny S. Ghate <tiny.ghate@gmail.com>
2016-07-19 10:14:24 +0530

    add actions to books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,29 @@
 class BooksController < ApplicationController
 	def index
 	end
+
+	def new
+	end
+
+	def create
+	end
+
+	def show
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+	private
+	def find_id
+		@book=Books.find(params[:id])
+	end
+	def books_params
+	end
 end
